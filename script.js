fe5f15deb1c27bc7c519823bddcc8034b4322c5b
Abdullah Siddiqui <abzsiddiqui@outlook.com>
2026-09-11 17:42:48 +0100

    Add human choice and global variables for scores

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
 function getComputerChoice() {
   let choice = Math.random();
   if (choice <= 0.33) {
@@ -8,10 +10,9 @@ function getComputerChoice() {
     return "Paper";
   }
 }
+function getHumanChoice() {
+  let choice = prompt("Please enter a choice from [Rock, Paper, Scissors]: ");
+  return choice;
+}
 
-console.log(getComputerChoice());
-
-// use math.random to return a number between 0 and 1
-// if number is less than or equal to 0.33: return Rock
-// elif number is greater than 0.33 and less than or equal to 0.66: return Scissors
-// else: return Paper
+console.log(getHumanChoice());
