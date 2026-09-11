61723ffa950de7696c36c96d9725cdc65abe8f96
Abdullah Siddiqui <abzsiddiqui@outlook.com>
2026-09-11 17:34:08 +0100

    Fix else if block

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getComputerChoice() {
   let choice = Math.random();
   if (choice <= 0.33) {
     return "Rock";
-  } else if (choice > 0.33 && choice <= 0.66) {
+  } else if (choice < 0.66) {
     return "Scissors";
   } else {
     return "Paper";
